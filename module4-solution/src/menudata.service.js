b4eff7a3a5f4cc4c53eecdcc576eef7061583dd0
Hesham <heshamusama@gmail.com>
2017-02-03 17:10:47 +0200

    module4-solution initial structure and names

---
+++ module4-solution/src/menudata.service.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular.module('data')
+    .service('MenuDataService', MenuDataService);
+
+
+  MenuDataService.$inject = ['$http', 'ApiBasePath'];
+  function MenuDataService($q, $timeout) {
+    var service = this;
+
+    // List of category items
+    var items = [];
+
+    service.getAllCategories = function() {
+      return $http({
+        method: "GET",
+        url: (ApiBasePath + "/categories.json")
+      });
+    };
+
+    service.getItemsForCategory = function(categoryShortName) {
+      var response = $http({
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json"),
+        params: {
+          category: categoryShortName
+        }
+      });
+
+      return response;
+    };
+  }
+
+})();
